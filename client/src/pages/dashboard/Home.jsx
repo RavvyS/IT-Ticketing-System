824f8050a5a3ae99434f50df5031268e27686677
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-08 23:07:19 +0530

    new

--- client/src/pages/dashboard/Home.jsx
+++ client/src/pages/dashboard/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import BasicBars from '../../components/other/BasicBars'
+import Card from '@mui/joy/Card';
 
 const Home = () => {
   return (
-    <div>sd</div>
+    <div>
+      <BasicBars />
+      <Card size="lg">Large card</Card>
+    </div>
   )
 }
 
