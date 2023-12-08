56f74364a0c29ff56a7c424675a578b032471fb3
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-08 18:48:10 +0530

    navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,19 +2,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Home from "./pages/dashboard/Home";
 import Sidebar from "./components/common/Sidebar";
+import Navbar from "./components/common/Navbar";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/dashboard/home" element={<Home />} />
-        </Routes>
+       
+          {window.location.pathname !== "/" && <Navbar />}
+       
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/dashboard/home" element={<Home />} />
+          </Routes>
+    
       </BrowserRouter>
-      <div className="flex">
-        {window.location.pathname !== "/" && <Sidebar />}
-      </div>
     </>
   );
 }
