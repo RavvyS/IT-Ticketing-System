445688af8145757814d920849709612ef3341bbe
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-09 00:11:36 +0530

    latest

--- client/src/pages/dashboard/Home.jsx
+++ client/src/pages/dashboard/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import BasicBars from "../../components/other/BasicBars";
 import Card from "@mui/joy/Card";
-import Typography from '@mui/joy/Typography';
+import Typography from "@mui/joy/Typography";
 import TicketCardContainer from "../../components/containers/TicketCardContainer";
 import SimpleLineChart from "../../components/other/SimpleLineChart";
+import HomeTable from "../../components/other/HomeTable";
 
 const Home = () => {
   return (
@@ -11,23 +12,28 @@ const Home = () => {
       <TicketCardContainer />
 
       <div class="flex justify-end items-center m-7">
+        <div>
+          <Card size="lg">
+            <HomeTable />
+          </Card>
+        </div>
+
         <div class="bg-white p-4 rounded-lg shadow-lg">
           <Card size="lg">
-          <Typography level="title-lg">Monthly Sales Figures </Typography>
+            <Typography level="title-lg">Monthly Sales Figures </Typography>
             <SimpleLineChart />
           </Card>
         </div>
-      
-      <div class="flex justify-start items-center m-7">
-        <div class="bg-white p-4 rounded-lg shadow-lg">
-          <Card size="lg">
-          <Typography level="title-lg">Annual Raised Tickets</Typography>
-            <BasicBars />
-          </Card>
+
+        <div class="flex justify-start items-center m-7">
+          <div class="bg-white p-4 rounded-lg shadow-lg">
+            <Card size="lg">
+              <Typography level="title-lg">Annual Raised Tickets</Typography>
+              <BasicBars />
+            </Card>
+          </div>
         </div>
       </div>
-      </div>
-
     </div>
   );
 };
