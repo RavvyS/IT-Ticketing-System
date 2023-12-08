50bc50c26585a56a12219ce1ceb4acdd1b8d651a
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-08 17:22:19 +0530

    ok

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Home from "./pages/dashboard/Home";
+import Sidebar from "./components/common/Sidebar";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Landing />} />
-          <Route path="/dashboard/home" element={<Home/>} />
+          <Route path="/dashboard/home" element={<Home />} />
         </Routes>
       </BrowserRouter>
+      <div className="flex">
+        {window.location.pathname !== "/" && <Sidebar />}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
