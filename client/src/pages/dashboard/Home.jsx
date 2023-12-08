12234c1d5acb81676c576a0ead3f1465703a46cc
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-08 23:53:34 +0530

    chart

--- client/src/pages/dashboard/Home.jsx
+++ client/src/pages/dashboard/Home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import BasicBars from "../../components/other/BasicBars";
 import Card from "@mui/joy/Card";
+import Typography from '@mui/joy/Typography';
 import TicketCardContainer from "../../components/containers/TicketCardContainer";
+import SimpleLineChart from "../../components/other/SimpleLineChart";
 
 const Home = () => {
   return (
@@ -11,9 +13,20 @@ const Home = () => {
       <div class="flex justify-end items-center m-7">
         <div class="bg-white p-4 rounded-lg shadow-lg">
           <Card size="lg">
+          <Typography level="title-lg">Annual Raised Tickets</Typography>
             <BasicBars />
           </Card>
         </div>
+      
+
+      <div class="flex justify-start items-center m-7">
+        <div class="bg-white p-4 rounded-lg shadow-lg">
+          <Card size="lg">
+          <Typography level="title-lg">Monthly Sales Figurs</Typography>
+            <SimpleLineChart />
+          </Card>
+        </div>
+      </div>
       </div>
 
     </div>
