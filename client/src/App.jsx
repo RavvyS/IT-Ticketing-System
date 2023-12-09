a834c09b75120f567ec5b61c1eca81c4c6b23301
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-09 13:58:27 +0530

    redo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Home from "./pages/dashboard/Home";
-import Sidebar from "./components/common/Sidebar";
-import Navbar from "./components/common/Navbar";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        {/* {window.location.pathname !== "/" && <Navbar />} */}
         <Routes>
           <Route path="/" element={<Landing />} />
-          <Route path="/dashboard/home" element={<Home />} />
+          <Route path="/dashboard/home" element={<Home/>} />
         </Routes>
       </BrowserRouter>
     </>
--- client/src/pages/dashboard/Home.jsx
+++ client/src/pages/dashboard/Home.jsx
@@ -1,48 +1,9 @@
-import React from "react";
-import BasicBars from "../../components/other/BasicBars";
-import Card from "@mui/joy/Card";
-import Typography from "@mui/joy/Typography";
-import TicketCardContainer from "../../components/containers/TicketCardContainer";
-import SimpleLineChart from "../../components/other/SimpleLineChart";
-import HomeTable from "../../components/other/HomeTable";
-import Sidebar from "../../components/common/Sidebar";
+import React from 'react'
 
 const Home = () => {
   return (
-    <div className="flex flex-col md:flex-row">
+    <div>Home</div>
+  )
+}
 
-      {/* Sidebar */}
-      <Sidebar />
-
-      <div className="p-4 md:p-7 text-2xl font-semibold flex-1 h-screen">
-
-        {/* Main content area */}
-        <TicketCardContainer />
-
-        <div className="md:flex justify-between items-center m-4 md:m-5">
-          <div className="md:w-1/2 md:mr-3 bg-white p-4 rounded-lg shadow-lg">
-            <Card size="lg">
-              <Typography level="title-lg">Monthly Sales Figures</Typography>
-              <SimpleLineChart />
-            </Card>
-          </div>
-
-          <div className="md:w-1/2 md:ml-3 bg-white p-4 rounded-lg shadow-lg">
-            <Card size="lg">
-              <Typography level="title-lg">Annual Raised Tickets</Typography>
-              <BasicBars />
-            </Card>
-          </div>
-        </div>
-
-        <div className="md:flex justify-start items-center m-4 md:m-5">
-          <Card size="lg">
-            <HomeTable />
-          </Card>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Home;
+export default Home
