56be0afc80b5d1822a19dcd273a33ef42e0c18dc
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-10 09:43:03 +0530

    charts

--- client/src/data/data.js
+++ client/src/data/data.js
@@ -1,8 +1,32 @@
-export const MonthlyTicketSummary = [
-    {
-      id: 1,
-      completedTickets: 50,
-      pendingTickets: 55,
-    },
-  ];
-  
+export const YearlyTicketSummary = [
+  {
+    id: 1,
+    month: "January",
+    completedTickets: 50,
+    pendingTickets: 55,
+  },
+  {
+    id: 2,
+    month: "February",
+    completedTickets: 20,
+    pendingTickets: 30,
+  },
+  {
+    id: 3,
+    month: "March",
+    completedTickets: 50,
+    pendingTickets: 50,
+  },
+  {
+    id: 4,
+    month: "April",
+    completedTickets: 80,
+    pendingTickets: 80,
+  },
+  {
+    id: 5,
+    month: "May",
+    completedTickets: 10,
+    pendingTickets: 35,
+  },
+];
--- client/src/pages/dashboard/Home.jsx
+++ client/src/pages/dashboard/Home.jsx
@@ -1,10 +1,50 @@
 import React from "react";
+import { useState } from "react";
+import PieChart from "../../components/other/PieChart";
 import SidebarLayout from "../../components/Layouts/SidebarLayout";
 
+import { Card, CardBody } from "@chakra-ui/react";
+
 const Home = () => {
+  const [ticketSummary, setTicketSummary] = useState({
+    labels: ["Completed", "Pending"],
+    datasets: [
+      {
+        label: ["Completed", "Pending"],
+        data: [50, 80],
+        fill: false,
+        backgroundColor: ["rgba(75, 192, 192, 0.2)", "rgba(255, 99, 132, 0.2)"],
+        borderColor: ["rgb(75, 192, 192)", "rgb(255, 99, 132)"],
+        borderWidth: 1,
+      },
+    ],
+  });
+
   return (
     <div>
-      <SidebarLayout></SidebarLayout>
+      <SidebarLayout>
+
+        <div className="flex space-x-5">
+
+          <div className="w-96 shadow-lg m-5">
+            <Card>
+              <CardBody>
+                <PieChart chartData={ticketSummary} className="w-full" />
+              </CardBody>
+            </Card>
+          </div>
+
+          <div className="w-96 shadow-lg m-5">
+            <Card>
+              <CardBody>
+                <PieChart chartData={ticketSummary} className="w-full" />
+              </CardBody>
+            </Card>
+          </div>
+          
+        </div>
+
+      </SidebarLayout>
     </div>
   );
 };
