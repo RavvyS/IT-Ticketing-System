69e54001487a4596f3d5a6028ed1c0b8f7ac6c18
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-10 08:50:02 +0530

    new

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -4,24 +4,24 @@ const Sidebar = () => {
   return (
     <div>
       <div className="flex flex-col space-y-4">
-        <aside class="flex flex-col w-64 h-screen px-5 py-8 overflow-y-auto bg-white border-r rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700">
+        <aside className="flex flex-col w-64 h-screen px-5 py-8 overflow-y-auto bg-white border-r rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700">
           <a href="#">
             <img
-              class="w-auto h-7"
+              className="w-auto h-7"
               src="https://merakiui.com/images/logo.svg"
               alt=""
             />
           </a>
 
-          <div class="flex flex-col justify-between flex-1 mt-6">
-            <nav class="-mx-3 space-y-6 ">
-              <div class="space-y-3 ">
-                <label class="px-3 text-xs text-gray-500 uppercase dark:text-gray-400">
+          <div className="flex flex-col justify-between flex-1 mt-6">
+            <nav className="-mx-3 space-y-6 ">
+              <div className="space-y-3 ">
+                <label className="px-3 text-xs text-gray-500 uppercase dark:text-gray-400">
                   analytics
                 </label>
 
                 <a
-                  class="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+                  className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
                   href="#"
                 >
                   <svg
@@ -30,7 +30,7 @@ const Sidebar = () => {
                     viewBox="0 0 24 24"
                     stroke-width="1.5"
                     stroke="currentColor"
-                    class="w-5 h-5"
+                    className="w-5 h-5"
                   >
                     <path
                       stroke-linecap="round"
@@ -39,11 +39,11 @@ const Sidebar = () => {
                     />
                   </svg>
 
-                  <span class="mx-2 text-sm font-medium">Dashboard</span>
+                  <span className="mx-2 text-sm font-medium">Dashboard</span>
                 </a>
 
                 <a
-                  class="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+                  className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
                   href="#"
                 >
                   <svg
@@ -52,7 +52,7 @@ const Sidebar = () => {
                     viewBox="0 0 24 24"
                     stroke-width="1.5"
                     stroke="currentColor"
-                    class="w-5 h-5"
+                    className="w-5 h-5"
                   >
                     <path
                       stroke-linecap="round"
@@ -61,17 +61,17 @@ const Sidebar = () => {
                     />
                   </svg>
 
-                  <span class="mx-2 text-sm font-medium">Preformance</span>
+                  <span className="mx-2 text-sm font-medium">Preformance</span>
                 </a>
               </div>
 
-              <div class="space-y-3 ">
-                <label class="px-3 text-xs text-gray-500 uppercase dark:text-gray-400">
+              <div className="space-y-3 ">
+                <label className="px-3 text-xs text-gray-500 uppercase dark:text-gray-400">
                   content
                 </label>
 
                 <a
-                  class="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+                  className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
                   href="#"
                 >
                   <svg
@@ -80,7 +80,7 @@ const Sidebar = () => {
                     viewBox="0 0 24 24"
                     stroke-width="1.5"
                     stroke="currentColor"
-                    class="w-5 h-5"
+                    className="w-5 h-5"
                   >
                     <path
                       stroke-linecap="round"
@@ -89,11 +89,11 @@ const Sidebar = () => {
                     />
                   </svg>
 
-                  <span class="mx-2 text-sm font-medium">Guides</span>
+                  <span className="mx-2 text-sm font-medium">Guides</span>
                 </a>
 
                 <a
-                  class="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+                  className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
                   href="#"
                 >
                   <svg
@@ -102,7 +102,7 @@ const Sidebar = () => {
                     viewBox="0 0 24 24"
                     stroke-width="1.5"
                     stroke="currentColor"
-                    class="w-5 h-5"
+                    className="w-5 h-5"
                   >
                     <path
                       stroke-linecap="round"
@@ -111,11 +111,11 @@ const Sidebar = () => {
                     />
                   </svg>
 
-                  <span class="mx-2 text-sm font-medium">Hotspots</span>
+                  <span className="mx-2 text-sm font-medium">Hotspots</span>
                 </a>
 
                 <a
-                  class="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+                  className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
                   href="#"
                 >
                   <svg
@@ -124,7 +124,7 @@ const Sidebar = () => {
                     viewBox="0 0 24 24"
                     stroke-width="1.5"
                     stroke="currentColor"
-                    class="w-5 h-5"
+                    className="w-5 h-5"
                   >
                     <path
                       stroke-linecap="round"
@@ -133,17 +133,17 @@ const Sidebar = () => {
                     />
                   </svg>
 
-                  <span class="mx-2 text-sm font-medium">Checklists</span>
+                  <span className="mx-2 text-sm font-medium">Checklists</span>
                 </a>
               </div>
 
-              <div class="space-y-3 ">
-                <label class="px-3 text-xs text-gray-500 uppercase dark:text-gray-400">
+              <div className="space-y-3 ">
+                <label className="px-3 text-xs text-gray-500 uppercase dark:text-gray-400">
                   Customization
                 </label>
 
                 <a
-                  class="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+                  className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
                   href="#"
                 >
                   <svg
@@ -152,7 +152,7 @@ const Sidebar = () => {
                     viewBox="0 0 24 24"
                     stroke-width="1.5"
                     stroke="currentColor"
-                    class="w-5 h-5"
+                    className="w-5 h-5"
                   >
                     <path
                       stroke-linecap="round"
@@ -161,11 +161,11 @@ const Sidebar = () => {
                     />
                   </svg>
 
-                  <span class="mx-2 text-sm font-medium">Themes</span>
+                  <span className="mx-2 text-sm font-medium">Themes</span>
                 </a>
 
                 <a
-                  class="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+                  className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
                   href="#"
                 >
                   <svg
@@ -174,7 +174,7 @@ const Sidebar = () => {
                     viewBox="0 0 24 24"
                     stroke-width="1.5"
                     stroke="currentColor"
-                    class="w-5 h-5"
+                    className="w-5 h-5"
                   >
                     <path
                       stroke-linecap="round"
@@ -188,7 +188,7 @@ const Sidebar = () => {
                     />
                   </svg>
 
-                  <span class="mx-2 text-sm font-medium">Setting</span>
+                  <span className="mx-2 text-sm font-medium">Setting</span>
                 </a>
               </div>
             </nav>
