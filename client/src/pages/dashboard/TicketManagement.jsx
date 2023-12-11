a153548c8c31bd4bcd6dab2e292c28926d6bea2a
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-11 21:24:44 +0530

    UI

--- client/src/pages/dashboard/TicketManagement.jsx
+++ client/src/pages/dashboard/TicketManagement.jsx
@@ -1,14 +1,65 @@
-import React from 'react'
-import SidebarLayout from '../../components/Layouts/SidebarLayout'
+import React from "react";
+import SidebarLayout from "../../components/Layouts/SidebarLayout";
+import TicketManagementTable from "../../components/other/TicketManagementTable";
+
+import { FaAngleRight, FaHouse } from "react-icons/fa6";
+import { TbReportAnalytics } from "react-icons/tb";
+
+import { Tooltip } from "react-tooltip";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+} from "@chakra-ui/react";
+
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 const TicketManagement = () => {
   return (
     <div>
-        <SidebarLayout> 
-            
-        </SidebarLayout>
-    </div>
-  )
-}
+      <SidebarLayout>
+        <div className="text-xs m-2">
+          <Breadcrumb
+            spacing="8px"
+            separator={<FaAngleRight color="gray.500" />}
+          >
+            <BreadcrumbItem>
+              <BreadcrumbLink href="#">
+                <FaHouse />
+              </BreadcrumbLink>
+            </BreadcrumbItem>
 
-export default TicketManagement
+            <BreadcrumbItem>
+              <BreadcrumbLink href="#">Dashboard</BreadcrumbLink>
+            </BreadcrumbItem>
+
+            <BreadcrumbItem>
+              <BreadcrumbLink href="#">Ticket Management</BreadcrumbLink>
+            </BreadcrumbItem>
+          </Breadcrumb>
+        </div>
+
+        <div className="ml-2 mt-8 flex items-center">
+          <h1 className="text-2xl font-bold">Ticket Management</h1>
+          <Tooltip id="report" style={{ borderRadius: "10px" }} place="left" />
+          <Button
+            colorScheme="teal"
+            size="md"
+            variant="outline"
+            className="ml-auto"
+            data-tooltip-id="report"
+            data-tooltip-content="Get Report"
+          >
+            <TbReportAnalytics />
+          </Button>
+        </div>
+
+        <TicketManagementTable />
+      </SidebarLayout>
+    </div>
+  );
+};
+
+export default TicketManagement;
