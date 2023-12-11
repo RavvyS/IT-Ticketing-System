9da097b79307b5502ed781febee4e92856d12a10
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-11 19:51:15 +0530

    icon

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {
-  FaEarthAsia,
+  FaTicket,
   FaChartColumn,
   FaFrog,
   FaJs,
@@ -42,7 +42,7 @@ const Sidebar = () => {
                   className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
                   href="/dashboard/ticketManagement"
                 >
-                  <FaEarthAsia />
+                  <FaTicket />
 
                   <span className="mx-2 text-sm font-medium">Ticket Management</span>
                 </a>
