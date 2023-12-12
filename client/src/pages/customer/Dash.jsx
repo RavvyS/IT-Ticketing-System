eca3f3e6a104f659e6b2e6cc890d802c01ead12c
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-12 11:58:44 +0530

    Nav

--- client/src/pages/customer/Dash.jsx
+++ client/src/pages/customer/Dash.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from '../../components/common/Navbar'
+
+
 
 const Dash = () => {
   return (
-    <div>Dash</div>
+    <div>
+        <Navbar/>
+    </div>
   )
 }
 
