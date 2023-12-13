c14904d23113ebc2bcf801db708e0676c0ca2b50
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-13 13:33:35 +0530

    model

--- server/models/userModel.ts
+++ server/models/userModel.ts
@@ -1,5 +1,6 @@
 import { connection } from "../db/dbConfig";
 import validator from "validator";
+import bcrypt from "bcrypt";
 
 const userModel = {
   createUser: (callback: any) => {
@@ -13,14 +14,23 @@ const userModel = {
         throw Error("Password not strong enouhg");
       }
 
-      const exists = connection.query("SELECT email FROM user WHERE email = ?", (error, results) => {
-        if (exists) {
-          throw Error(`Email: ${exists} already in use`);
+      const exists = connection.query(
+        "SELECT email FROM user WHERE email = ?",
+        async (error, results) => {
+          if (exists) {
+            throw Error(`Email: ${exists} already in use`);
+          }
+          const salt = await bcrypt.genSalt(10);
+          const hash = await bcrypt.hash(password, salt);
+
+          const user = connection.query("INSERT INTO user VALUES (?, ?)", {
+            email,
+            hash,
+          });
+
+          callback(error, results);
         }
-        const 
-        callback(error, results);
-      });
+      );
     };
   },
-
 };
