84c45fb8cc8f1fa7fca758b890ba9cb6b7356ab4
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-13 11:22:41 +0530

    init

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,21 @@
 import express, { Application, Express } from "express";
+import dotenv from "dotenv";
+
+// Middleware
+dotenv.config();
 
 const app: Application = express();
 
+const port: any = process.env.PORT || 9000;
+
+const start = async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is running on port: ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
