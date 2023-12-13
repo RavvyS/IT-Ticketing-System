51b1d3b9a2c3931271d052251f43a12d140536df
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-13 13:03:27 +0530

    index

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,16 @@
 import express, { Application, Express } from "express";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from 'cors'
+
+// Instances
+const app: Application = express();
 
 // Middleware
+app.use(bodyParser.json())
+app.use(cors())
 dotenv.config();
 
-const app: Application = express();
 
 const port: any = process.env.PORT || 9000;
 
