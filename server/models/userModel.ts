2028b4fa8e1b4c41914061cd9913b86db567316b
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-13 13:26:07 +0530

    bcrypt

--- server/models/userModel.ts
+++ server/models/userModel.ts
@@ -1,7 +1,26 @@
 import { connection } from "../db/dbConfig";
+import validator from "validator";
 
-const signup= async (email: string, password: string)  => {
+const userModel = {
+  createUser: (callback: any) => {
+    const signup = async (email: string, password: string) => {
+      // Validation
+      if (!email || !password) {
+        throw Error("All fields must be filled");
+      } else if (!validator.isEmail(email)) {
+        throw Error("Email is not valid");
+      } else if (!validator.isStrongPassword(password)) {
+        throw Error("Password not strong enouhg");
+      }
 
-    // Validation
-    if()
-}
+      const exists = connection.query("SELECT email FROM user WHERE email = ?", (error, results) => {
+        if (exists) {
+          throw Error(`Email: ${exists} already in use`);
+        }
+        const 
+        callback(error, results);
+      });
+    };
+  },
+
+};
