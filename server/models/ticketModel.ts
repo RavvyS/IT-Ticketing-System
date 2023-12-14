d6ef9fb16e2417e3356770bb4107350da387bbc2
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-14 21:27:37 +0530

    Schema

--- server/models/ticketModel.ts
+++ server/models/ticketModel.ts
@@ -1,9 +1,32 @@
-import { connection } from "../db/dbConfig";
+import { Schema } from "mongoose";
 
-const ticketModel = {
-  getTicket: (callback: any) => {
-    connection.query("SELECT * FROM ticket", (error, results) => {
-      callback(error, results);
-    });
+// Interface
+interface Ticket {
+  subject: string;
+  description: string;
+  urgency: boolean;
+  status: number;
+  created_at: Date;
+}
+
+// Schema
+const ticketSchema = new Schema<Ticket>({
+  subject: {
+    type: String,
+    // required: true
   },
-};
+  description: {
+    type: String,
+  },
+  urgency: {
+    type: Boolean,
+  },
+  status: {
+    type: Number,
+  },
+  created_at: {
+    type: Date,
+  },
+});
+
+export default ticketSchema;
