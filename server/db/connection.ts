ec759e2d7540dc49cb6d51a0385570e98a88a7fb
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-14 20:09:18 +0530

    latest

--- server/db/connection.ts
+++ server/db/connection.ts
@@ -6,7 +6,7 @@ const connection = async (url: any) => {
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB: ", error);
-    throw error; // Re-throw the error to propagate it to the caller
+    throw error;
   }
 };
 
