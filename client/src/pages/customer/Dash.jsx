4c330b22eac7c0454fc15f6b30ab90264a756543
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-15 16:40:42 +0530

    latest

--- client/src/pages/customer/Dash.jsx
+++ client/src/pages/customer/Dash.jsx
@@ -13,68 +13,55 @@ const Dash = () => {
     <div>
       <Navbar />
 
-      <Tabs variant="soft-rounded" colorScheme="green">
-        <TabList>
-          <Tab>View</Tab>
-          <Tab>Create</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
-            <div className="mt-10 mx-10">
-              <Flex>
-                <Input
-                  flex="4"
-                  mr="2"
-                  variant="outline"
-                  size="sm"
-                  placeholder="Search"
-                  rounded="md"
-                />
-                <Select
-                  flex="1"
-                  mr="2"
-                  variant="outline"
-                  size="sm"
-                  placeholder="Filter"
-                  rounded="md"
-                />
-                <Select
-                  flex="1"
-                  mr="2"
-                  variant="outline"
-                  size="sm"
-                  placeholder="Number"
-                  rounded="md"
-                />
-                <Select
-                  flex="1"
-                  variant="outline"
-                  size="sm"
-                  placeholder="Choose"
-                  rounded="md"
-                />
-              </Flex>
-            </div>
+      <div className="mt-10 mx-10">
+        <Flex>
+          <Input
+            flex="4"
+            mr="2"
+            variant="outline"
+            size="sm"
+            placeholder="Search"
+            rounded="md"
+          />
+          <Select
+            flex="1"
+            mr="2"
+            variant="outline"
+            size="sm"
+            placeholder="Filter"
+            rounded="md"
+          />
+          <Select
+            flex="1"
+            mr="2"
+            variant="outline"
+            size="sm"
+            placeholder="Number"
+            rounded="md"
+          />
+          <Select
+            flex="1"
+            variant="outline"
+            size="sm"
+            placeholder="Choose"
+            rounded="md"
+          />
+        </Flex>
+      </div>
 
-            <CustomerTicketTable />
+      <CustomerTicketTable />
 
-            <div className="flex items-center justify-center text-center mt-5">
-              <Button colorScheme="teal" variant="outline" className="m-5">
-                Create a Ticket
-              </Button>
-              <Button colorScheme="teal" variant="outline" className="m-5">
-                Create a Ticket
-              </Button>
-              <Button colorScheme="teal" variant="outline" className="m-5">
-                Create a Ticket
-              </Button>
-            </div>
-          </TabPanel>
-          <TabPanel>
-            <p>two!</p>
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
+      <div className="flex items-center justify-center text-center mt-5">
+        <Button colorScheme="teal" variant="outline" className="m-5">
+          Create a Ticket
+        </Button>
+        <Button colorScheme="teal" variant="outline" className="m-5">
+          Create a Ticket
+        </Button>
+        <Button colorScheme="teal" variant="outline" className="m-5">
+          Create a Ticket
+        </Button>
+      </div>
     </div>
   );
 };
