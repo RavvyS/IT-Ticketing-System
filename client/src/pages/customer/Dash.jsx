470c8818f1593d080577629ea246860041e4b2b4
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-16 12:12:01 +0530

    latest

--- client/src/pages/customer/Dash.jsx
+++ client/src/pages/customer/Dash.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/common/Navbar";
 import CustomerTicketTable from "../../components/other/CustomerTicketTable";
 
@@ -9,59 +9,88 @@ import { Flex } from "@chakra-ui/react";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 
 const Dash = () => {
+  const [activeTab, setActiveTab] = useState(1);
+
+  const handleTabClick = (tabNumber) => {
+    setActiveTab(tabNumber);
+  };
+
   return (
     <div>
       <Navbar />
 
-      <div className="mt-10 mx-10">
-        <Flex>
-          <Input
-            flex="4"
-            mr="2"
-            variant="outline"
-            size="sm"
-            placeholder="Search"
-            rounded="md"
-          />
-          <Select
-            flex="1"
-            mr="2"
-            variant="outline"
-            size="sm"
-            placeholder="Filter"
-            rounded="md"
-          />
-          <Select
-            flex="1"
-            mr="2"
-            variant="outline"
-            size="sm"
-            placeholder="Number"
-            rounded="md"
-          />
-          <Select
-            flex="1"
-            variant="outline"
-            size="sm"
-            placeholder="Choose"
-            rounded="md"
-          />
-        </Flex>
+      <div className="flex justify-center items-center mb-5">
+        <div role="tablist" className="tabs tabs-boxed">
+          <a
+            role="tab"
+            className={`tab ${activeTab === 1 ? "tab-active" : ""}`}
+            onClick={() => handleTabClick(1)}
+          >
+            View Tickets
+          </a>
+          <a
+            role="tab"
+            className={`tab ${activeTab === 2 ? "tab-active" : ""}`}
+            onClick={() => handleTabClick(2)}
+          >
+            Create Ticket
+          </a>
+        </div>
       </div>
 
-      <CustomerTicketTable />
+      {activeTab === 1 && (
+        <div className="mt-10 mx-10">
+          <Flex>
+            <Input
+              flex="4"
+              mr="2"
+              variant="outline"
+              size="sm"
+              placeholder="Search"
+              rounded="md"
+            />
+            <Select
+              flex="1"
+              mr="2"
+              variant="outline"
+              size="sm"
+              placeholder="Filter"
+              rounded="md"
+            />
+            <Select
+              flex="1"
+              mr="2"
+              variant="outline"
+              size="sm"
+              placeholder="Number"
+              rounded="md"
+            />
+            <Select
+              flex="1"
+              variant="outline"
+              size="sm"
+              placeholder="Choose"
+              rounded="md"
+            />
+          </Flex>
 
-      <div className="flex items-center justify-center text-center mt-5">
-        <Button colorScheme="teal" variant="outline" className="m-5">
-          Create a Ticket
-        </Button>
-        <Button colorScheme="teal" variant="outline" className="m-5">
-          Create a Ticket
-        </Button>
-        <Button colorScheme="teal" variant="outline" className="m-5">
-          Create a Ticket
-        </Button>
-      </div>
+          <CustomerTicketTable />
+
+          <div className="flex items-center justify-center text-center mt-5">
+            <Button colorScheme="teal" variant="outline" className="m-5">
+              Create a Ticket
+            </Button>
+            <Button colorScheme="teal" variant="outline" className="m-5">
+              Create a Ticket
+            </Button>
+            <Button colorScheme="teal" variant="outline" className="m-5">
+              Create a Ticket
+            </Button>
+          </div>
+        </div>
+      )}
+
+      {activeTab === 2 && <h1>tabs2</h1>}
     </div>
   );
 };
