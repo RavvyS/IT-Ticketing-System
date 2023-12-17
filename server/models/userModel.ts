88dbc1907ac1986457163fcf120e44f765d01978
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-17 20:06:32 +0530

    userModel

--- server/models/userModel.ts
+++ server/models/userModel.ts
@@ -1,37 +1,20 @@
-import { connection } from "../db/dbConfig";
-import validator from "validator";
-import bcrypt from "bcrypt";
+import { Schema, model } from "mongoose";
 
-const userModel = {
-  createUser: async (email: string, password: string) => {
-    // Validation
-    if (!email || !password) {
-      throw Error("All fields must be filled");
-    } else if (!validator.isEmail(email)) {
-      throw Error("Email is not valid");
-    } else if (!validator.isStrongPassword(password)) {
-      throw Error("Password not strong enough");
-    }
+interface User {
+  email: string;
+  password: string;
+}
 
-    const existingUser = await connection.query(
-      "SELECT email FROM user WHERE email = ?",
-      [email]
-    );
-
-    if (existingUser.length > 0) {
-      throw Error(`Email: ${email} already in use`);
-    }
-
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-
-    const newUser = await connection.query(
-      "INSERT INTO user (email, password) VALUES (?, ?)",
-      [email, hash]
-    );
-
-    return newUser;
+const userSchema = new Schema<User>({
+  email: {
+    type: String,
+    required: true,
   },
-};
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
+const userModel = model("User", userSchema);
 export default userModel;
