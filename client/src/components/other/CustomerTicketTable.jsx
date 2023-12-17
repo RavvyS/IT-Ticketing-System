d1f49bf8cb10a5922bb29a2280f14749c5da0fae
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-17 18:43:09 +0530

    data loading

--- client/src/components/other/CustomerTicketTable.jsx
+++ client/src/components/other/CustomerTicketTable.jsx
@@ -37,8 +37,8 @@ const CustomerTicketTable = () => {
           <Thead className="bg-gray-200 border-b">
             <Tr>
               <Th>Subject</Th>
-              <Th>Urgent</Th>
               <Th>Status</Th>
+              <Th>Urgent</Th>
               <Th>Issued Date</Th>
               <Th></Th>
             </Tr>
@@ -47,9 +47,9 @@ const CustomerTicketTable = () => {
             {tableData.map((ticket) => (
               <Tr key={ticket._id}>
                 <Td>{ticket.subject}</Td>
-                <Td>{ticket.subject}</Td>
-                <Td>{ticket.subject}</Td>
-                <Td>{ticket.subject}</Td>
+                <Td>{ticket.status}</Td>
+                <Td>{ticket.urgency}Yes</Td>
+                <Td>{ticket.created_at}</Td>
                 <Td>
                   <Button colorScheme="teal" variant="outline">
                     Edit
