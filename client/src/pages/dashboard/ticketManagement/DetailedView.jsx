ce6d019d8e30ec9a46e48d2ce9b00a89bd788368
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-20 21:46:54 +0530

    new

--- client/src/pages/dashboard/ticketManagement/DetailedView.jsx
+++ client/src/pages/dashboard/ticketManagement/DetailedView.jsx
@@ -12,7 +12,16 @@ import {
   Heading,
   SimpleGrid,
   Text,
+  Center,
 } from "@chakra-ui/react";
+import { Avatar, AvatarBadge, AvatarGroup } from "@chakra-ui/react";
+import {
+    Tag,
+    TagLabel,
+    TagLeftIcon,
+    TagRightIcon,
+    TagCloseButton,
+  } from '@chakra-ui/react'
 import { FaAngleRight, FaHouse } from "react-icons/fa6";
 
 const DetailedView = () => {
@@ -47,7 +56,7 @@ const DetailedView = () => {
 
         {/* Cards */}
         <div className="flex my-5 ">
-          <div class="max-w-sm rounded-xl overflow-hidden shadow-lg mr-5">
+          <div class="max-w-sm rounded-xl overflow-hidden shadow-lg mt-20 mr-5">
             <img
               class="w-full"
               src="https://v1.tailwindcss.com/img/card-top.jpg"
@@ -74,36 +83,38 @@ const DetailedView = () => {
             </div>
           </div>
 
-
-            {/* Ticket sub/Desc and details */}
-          <div class="max-w-sm rounded-xl overflow-hidden shadow-lg mr-5">
-            <img
-              class="w-full"
-              src="https://v1.tailwindcss.com/img/card-top.jpg"
-              alt="Sunset in the mountains"
-            />
-            <div class="px-6 py-4">
-              <div class="font-bold text-xl mb-2">General details</div>
-              <p>Subject:</p>
-              <p>Description:</p>
-              <p>urgency:</p>
-              <p>current status:</p>
-              <p>created_at:</p>
+          {/* Ticket sub/Desc and details */}
+          <div class="max-w-sm mx-auto rounded-xl overflow-hidden shadow-lg">
+            <div>
+              <Center>
+                <Avatar name="Dan Abrahmov" src="https://bit.ly/dan-abramov" />
+              </Center>
             </div>
-            <div class="px-6 pt-4 pb-2">
-              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                #photography
+            <div class="px-6 py-4 text-center">
+              <div class="font-bold text-xl mb-2">General details</div>
+              <h3>2023/11/10</h3><br></br>
+              <p className="underline">Windows not loading</p><br></br>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                exercitationem praesentium nihil.
+              </p><br></br>
+            </div>
+            <div class="px-6 pt-4 pb-2 text-center">
+              <span class="inline-block mr-2 mb-2">
+              <Tag>Urgent : Chill</Tag>
               </span>
-              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                #travel
+              <span class="inline-block mr-2 mb-2">
+              <Tag>Status</Tag>
               </span>
-              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                #winter
+              <span class="inline-block mr-2 mb-2">
+              <Tag>Company</Tag>
               </span>
             </div>
           </div>
+          {/* end */}
 
-          <div class="max-w-sm rounded-xl overflow-hidden shadow-lg">
+          <div class="max-w-sm rounded-xl overflow-hidden shadow-lg mt-20">
             <img
               class="w-full"
               src="https://v1.tailwindcss.com/img/card-top.jpg"
