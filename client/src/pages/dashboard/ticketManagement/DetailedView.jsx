5b3c1670178cc29ad19b107ffb1c4d8fa4d56dc7
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-20 21:28:08 +0530

    new

--- client/src/pages/dashboard/ticketManagement/DetailedView.jsx
+++ client/src/pages/dashboard/ticketManagement/DetailedView.jsx
@@ -3,13 +3,22 @@ import React from "react";
 import SidebarLayout from "../../../components/Layouts/SidebarLayout";
 
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from "@chakra-ui/react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Button,
+  Heading,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
 import { FaAngleRight, FaHouse } from "react-icons/fa6";
 
 const DetailedView = () => {
   return (
     <div>
       <SidebarLayout>
-
         <div className="text-xs m-2">
           <Breadcrumb
             spacing="8px"
@@ -32,8 +41,95 @@ const DetailedView = () => {
           </Breadcrumb>
         </div>
 
-        
+        <div className="ml-2 mt-8 flex items-center">
+          <h1 className="text-2xl font-bold">Detailed View</h1>
+        </div>
 
+        {/* Cards */}
+        <div className="flex my-5 ">
+          <div class="max-w-sm rounded-xl overflow-hidden shadow-lg mr-5">
+            <img
+              class="w-full"
+              src="https://v1.tailwindcss.com/img/card-top.jpg"
+              alt="Sunset in the mountains"
+            />
+            <div class="px-6 py-4">
+              <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+              <p class="text-gray-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                exercitationem praesentium nihil.
+              </p>
+            </div>
+            <div class="px-6 pt-4 pb-2">
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #photography
+              </span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #travel
+              </span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #winter
+              </span>
+            </div>
+          </div>
+
+
+            {/* Ticket sub/Desc and details */}
+          <div class="max-w-sm rounded-xl overflow-hidden shadow-lg mr-5">
+            <img
+              class="w-full"
+              src="https://v1.tailwindcss.com/img/card-top.jpg"
+              alt="Sunset in the mountains"
+            />
+            <div class="px-6 py-4">
+              <div class="font-bold text-xl mb-2">General details</div>
+              <p>Subject:</p>
+              <p>Description:</p>
+              <p>urgency:</p>
+              <p>current status:</p>
+              <p>created_at:</p>
+            </div>
+            <div class="px-6 pt-4 pb-2">
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #photography
+              </span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #travel
+              </span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #winter
+              </span>
+            </div>
+          </div>
+
+          <div class="max-w-sm rounded-xl overflow-hidden shadow-lg">
+            <img
+              class="w-full"
+              src="https://v1.tailwindcss.com/img/card-top.jpg"
+              alt="Sunset in the mountains"
+            />
+            <div class="px-6 py-4">
+              <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+              <p class="text-gray-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                exercitationem praesentium nihil.
+              </p>
+            </div>
+            <div class="px-6 pt-4 pb-2">
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #photography
+              </span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #travel
+              </span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #winter
+              </span>
+            </div>
+          </div>
+        </div>
       </SidebarLayout>
     </div>
   );
