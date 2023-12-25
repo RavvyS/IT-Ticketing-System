f404df9757863f40b802910865241b8b732782f0
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-26 00:35:00 +0530

    UI

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -2,11 +2,6 @@ import React from "react";
 import {
   FaTicket,
   FaChartColumn,
-  FaFrog,
-  FaJs,
-  FaRegFaceGrinBeam,
-  FaHive,
-  FaEmpire,
 } from "react-icons/fa6";
 
 const Sidebar = () => {
@@ -45,63 +40,6 @@ const Sidebar = () => {
                   <FaTicket />
 
                   <span className="mx-2 text-sm font-medium">Ticket Management</span>
-                </a>
-              </div>
-
-              <div className="space-y-3 ">
-                <label className="px-3 text-xs text-gray-500 uppercase dark:text-gray-400">
-                  content
-                </label>
-
-                <a
-                  className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
-                  href="#"
-                >
-                  <FaFrog />
-
-                  <span className="mx-2 text-sm font-medium">Guides</span>
-                </a>
-
-                <a
-                  className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
-                  href="#"
-                >
-                  <FaJs />
-
-                  <span className="mx-2 text-sm font-medium">Hotspots</span>
-                </a>
-
-                <a
-                  className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
-                  href="#"
-                >
-                  <FaRegFaceGrinBeam />
-
-                  <span className="mx-2 text-sm font-medium">Checklists</span>
-                </a>
-              </div>
-
-              <div className="space-y-3 ">
-                <label className="px-3 text-xs text-gray-500 uppercase dark:text-gray-400">
-                  Customization
-                </label>
-
-                <a
-                  className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
-                  href="#"
-                >
-                  <FaHive />
-
-                  <span className="mx-2 text-sm font-medium">Themes</span>
-                </a>
-
-                <a
-                  className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
-                  href="#"
-                >
-                  <FaEmpire />
-
-                  <span className="mx-2 text-sm font-medium">Setting</span>
                 </a>
               </div>
             </nav>
--- client/src/pages/dashboard/Home.jsx
+++ client/src/pages/dashboard/Home.jsx
@@ -11,7 +11,6 @@ import {
 } from "@chakra-ui/react";
 
 import PieChart from "../../components/other/PieChart";
-import LineChart from "../../components/other/LineChart";
 
 import SidebarLayout from "../../components/Layouts/SidebarLayout";
 
@@ -36,36 +35,6 @@ const Home = () => {
         fill: false,
         backgroundColor: ["rgba(75, 192, 192, 0.2)", "rgba(255, 99, 132, 0.2)"],
         borderColor: ["rgb(75, 192, 192)", "rgb(255, 99, 132)"],
-        borderWidth: 1,
-      },
-    ],
-  });
-
-  const [anal, setAnal] = useState({
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-      {
-        label: ["Data 1"],
-        data: [50, 60, 20, 80, 50, 100],
-        fill: false,
-        backgroundColor: ["rgba(75, 192, 192, 0.2)"],
-        borderColor: ["rgb(75, 192, 192)"],
-        borderWidth: 1,
-      },
-      {
-        label: ["Data 2"],
-        data: [10, 80, 70, 65, 50, 30],
-        fill: false,
-        backgroundColor: ["rgb(255, 99, 132, 0.2)"],
-        borderColor: ["rgb(255, 99, 132)"],
-        borderWidth: 1,
-      },
-      {
-        label: ["Data 3"],
-        data: [10, 20, 30, 15, 80, 80],
-        fill: false,
-        backgroundColor: ["rgb(255, 159, 64, 0.2)"],
-        borderColor: ["rgb(255, 159, 64)"],
         borderWidth: 1,
       },
     ],
@@ -117,14 +86,6 @@ const Home = () => {
         </StatGroup>
         </CardBody>
         </Card>
-
-        <div className="m-5">
-          <Card>
-            <CardBody>
-              <LineChart chartData={anal} />
-            </CardBody>
-          </Card>
-        </div>
 
         <div className="flex space-x-5">
           <div className="w-96 shadow-lg m-5">
