e943aed07eee2d8c9f9045719891a998d160aae5
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-12-26 00:32:11 +0530

    UI

--- client/src/pages/dashboard/ticketManagement/DetailedView.jsx
+++ client/src/pages/dashboard/ticketManagement/DetailedView.jsx
@@ -26,23 +26,14 @@ import {
   Stepper,
   useSteps,
   Box,
-
+  CardBody,
+  Card,
+  Button
 } from "@chakra-ui/react";
 import { FaAngleRight, FaHouse } from "react-icons/fa6";
 import { FaAndroid } from "react-icons/fa6";
 
-const steps = [
-  { title: "First", description: "Analyze Ticket" },
-  { title: "Second", description: "Categorize Ticket" },
-  { title: "Third", description: "Select State" },
-];
-
 const DetailedView = () => {
-  const { activeStep } = useSteps({
-    index: 1,
-    count: steps.length,
-  });
-
   return (
     <div>
       <SidebarLayout>
@@ -72,40 +63,9 @@ const DetailedView = () => {
           <h1 className="text-2xl font-bold">Detailed View</h1>
         </div>
 
-        {/* Cards */}
-        <div className="flex my-5">
-          <div class="max-w-sm rounded-xl overflow-hidden shadow-lg mt-20 mr-5">
+        <Card className="mt-10">
+          <CardBody>
             <div class="px-6 py-4 text-center">
-              <div class="font-bold text-xl mb-5">Ticket categorization</div>
-              <FormControl>
-                <FormLabel>Issue relation</FormLabel>
-                <Select className="mb-5" placeholder="Select">
-                  <option>Networking</option>
-                  <option>Software</option>
-                  <option>OS</option>
-                  <option>Server</option>
-                  <option>Hardware</option>
-                  <option>Other</option>
-                </Select>
-
-                <FormLabel>Diagnosis</FormLabel>
-                <Textarea
-                  size="mb"
-                  placeholder="Enter the high level diagnostic"
-                />
-              </FormControl>
-            </div>
-          </div>
-
-          {/* Ticket sub/Desc and details */}
-          <div class="max-w-sm mx-auto rounded-xl overflow-hidden shadow-lg">
-            <div>
-              <Center>
-                <Avatar name="Dan Abrahmov" src="https://bit.ly/dan-abramov" />
-              </Center>
-            </div>
-            <div class="px-6 py-4 text-center">
-              <div class="font-bold text-xl mb-2">General details</div>
               <h3>2023/11/10</h3>
               <br></br>
               <p className="underline">Windows not loading</p>
@@ -128,52 +88,23 @@ const DetailedView = () => {
                 <Tag>Company</Tag>
               </span>
             </div>
-          </div>
-          {/* end */}
+          </CardBody>
+        </Card>
 
-          <div class="max-w-sm rounded-xl overflow-hidden shadow-lg mt-20">
-            <div class="px-6 py-4">
-              <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-              <p class="text-gray-700 text-base">
-                Lorem ipsum dolor st amet, consectetur adipisicing elit.
-                Voluptatibus quia, nulla! Maiores et perferendis eaque,
-                exercitationem praesentium nihil.
-              </p>
-            </div>
-            <div class="px-6 pt-4 pb-2">
-              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                #photography
-              </span>
-              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                #travel
-              </span>
-              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                #winter
-              </span>
-            </div>
-          </div>
-        </div>
-
-        <Stepper index={activeStep} className="mt-20">
-          {steps.map((step, index) => (
-            <Step key={index}>
-              <StepIndicator>
-                <StepStatus
-                  complete={<StepNumber />}
-                  incomplete={<StepNumber />}
-                  active={<StepNumber />}
-                />
-              </StepIndicator>
-
-              <Box flexShrink="0">
-                <StepTitle>{step.title}</StepTitle>
-                <StepDescription>{step.description}</StepDescription>
-              </Box>
-
-              <StepSeparator />
-            </Step>
-          ))}
-        </Stepper>
+        <Card>
+          <CardBody>
+            <FormControl>
+              <FormLabel>Change state of ticket</FormLabel>
+              <Select placeholder="Select">
+                <option>Working on</option>
+                <option>Canceled</option>
+                <option>Done</option>
+                <option>Pending</option>
+              </Select>
+              <Button className="mt-3" colorScheme="teal">Change</Button>
+            </FormControl>
+          </CardBody>
+        </Card>
       </SidebarLayout>
     </div>
   );
