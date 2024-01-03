98a7b19fcb91c5c0418a9e6fa81dd50634d37f90
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-01-03 14:43:55 +0530

    frontend-auth

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App.jsx";
 import "./index.css";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <AuthContextProvider>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
